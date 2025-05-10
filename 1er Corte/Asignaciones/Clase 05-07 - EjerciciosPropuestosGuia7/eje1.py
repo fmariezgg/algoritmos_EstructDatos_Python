d0526d1f5730082a3ea352f04ec186a514de4dae
Marie <fzogaib@uamv.edu.ni>
2025-05-10 09:48:49 -0600

    clase 05-07

---
+++ 1er Corte/Asignaciones/Clase 05-07 - EjerciciosPropuestosGuia7/eje1.py
@@ -0,0 +1,13 @@
+'''
+Integrantes
+Fatima Zogaib
+Andres Castillo
+
+Ejercicio 1.
+Una oficina de atención ciudadana en una alcaldía municipal en Nicaragua recibe documentos para
+revisión. Por cada solicitud, se apilan los documentos entregados en el orden en que llegan. El
+personal debe revisar el último documento entregado primero. Se debe simular el proceso de
+revisión, utilizando una pila, y permitir agregar nuevos documentos, eliminar el último revisado y
+mostrar los pendientes.
+'''
+
